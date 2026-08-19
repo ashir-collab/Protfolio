d4c0e4d96223931a5d7ab90eda9812c9087f6f19
Muhammad Aashir <muhammedashir41@gmail.com>
2026-08-19 19:56:47 +0500

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import Aboutme from "./Sections/Aboutme"
-import Experience from "./Sections/Experience"
-import Footer from "./Sections/Footer"
-import Input from "./common/Input"
-import Intro from "./Sections/Intro"
-import Project from "./Sections/Project"
-import Skills from "./Sections/Skills"
-
+import Aboutme from "./Sections/Aboutme";
+import Experience from "./Sections/Experience";
+import Footer from "./Sections/Footer";
+import Input from "./common/Input";
+import Intro from "./Sections/Intro";
+import Project from "./Sections/Project";
+import Skills from "./Sections/Skills";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-
   return (
     <>
-      <div id="intro"><Intro /></div>
-      <div id="experience"><Experience /></div>
-      <div id="aboutme"><Aboutme /></div>
-      <div id="skills"><Skills /></div>
-      <div id="project"><Project /></div>
-      <div id="input"><Input /></div>
-      <div><Footer /></div>
-
+      <div id="intro">
+        <Intro />
+      </div>
+      <div id="experience">
+        <Experience />
+      </div>
+      <div id="aboutme">
+        <Aboutme />
+      </div>
+      <div id="skills">
+        <Skills />
+      </div>
+      <div id="project">
+        <Project />
+      </div>
+      <div id="input">
+        <Input />
+      </div>
+      <div>
+        <Footer />
+      </div>
+      <Analytics />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
